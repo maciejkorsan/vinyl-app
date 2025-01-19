7d41078d4684a8a28df7af2f7ad309e805506821
Maciej Korsan <maciejkorsan@gmail.com>
2025-01-20 00:20:39 +0100

    add history controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get "logs/index"
   root "dashboard#index"
   get "dashboard/index"
   resource :session
   resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :logs
   resources :records
+  get "history", to: "history#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
