6cf3570efd444f3fc7ad4bdcdd8f71c13e80b3bb
Maciej Korsan <maciejkorsan@gmail.com>
2025-01-20 18:34:37 +0100

    total time formatter

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,8 @@
 module DashboardHelper
+  def format_time(time)
+    days = time / 86400
+    hours = (time % 86400) / 3600
+    minutes = (time % 3600) / 60
+    "#{days} day#{days > 1 || days == 0 ? "s" : ""} #{hours} hour#{hours > 1 || hours == 0 ? "s" : ""} #{minutes} minute#{minutes > 1 || minutes == 0 ? "s" : ""}  "
+  end
 end
