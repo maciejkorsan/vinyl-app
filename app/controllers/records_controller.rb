6bb519cac98d1bb639c9178a6b48f9173eba0403
Maciej Korsan <maciejkorsan@gmail.com>
2025-01-20 02:07:21 +0100

    Record creation form

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,22 @@
 class RecordsController < ApplicationController
   def index
+    @records = Record.all
   end
 
   def create
+    @record = Record.new(record_params)
+    @record.save
+    redirect_to records_path
+  end
+
+  def new
+    @record = Record.new
+  end
+
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :artist_id, :cover)
   end
 end
