252e49635bf9dcd7e07331a26e8f48b1e9b0170f
Maciej Korsan <maciejkorsan@gmail.com>
2025-01-26 16:22:43 +0100

    improve modals

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,8 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["modal", "close"]
+  
   connect() {
+    document.addEventListener("keydown", this.handleKeydown.bind(this))
   }
+
+  disconnect() {
+    document.removeEventListener("keydown", this.handleKeydown.bind(this))
+  }
+
+  handleKeydown(event) {
+    if (event.key === "Escape") {
+      this.close()
+    }
+  }
+
   close() {
     this.modalTarget.close()
   }
